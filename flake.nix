e85c37d0e75e8a67370887141df53c6b1f519ed8
Andrea Erario <andreaerario04@gmail.com>
2025-11-16 11:39:36 +0100

    Refactors to allow multiple hosts

--- flake.nix
+++ flake.nix
@@ -22,14 +22,13 @@
     nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./hosts/wsl.nix
 
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.sharedModules = [nvf.homeManagerModules.default];
-
           home-manager.users.andrea = import ./users/andrea;
         }
 
