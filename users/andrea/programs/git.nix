aeece49a690b7f88cbc21eb8410ede7bd020c84f
Andrea Erario <andreaerario04@gmail.com>
2025-11-18 14:40:10 +0100

    Fixes home-manager evaluation warnings

--- users/andrea/programs/git.nix
+++ users/andrea/programs/git.nix
@@ -5,8 +5,12 @@
 }: {
   programs.git = {
     enable = true;
-    userName = "Andrea Erario";
-    userEmail = "andreaerario04@gmail.com";
+    settings = {
+      user = {
+        name = "Andrea Erario";
+        email = "andreaerario04@gmail.com";
+      };
+    };
     signing = {
       key = "42D080106CF8AB07";
       signByDefault = true;
